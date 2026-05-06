e6006e948f42fea6529c04f395327d088a2bb512
Hyeoninii <hyeoninii0314@gmail.com>
2026-05-06 10:20:50 +0900

    feat:prettier 적용

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  trailingComma: "all",
+  proseWrap: "preserve",
+  vueIndentScriptAndStyle: false,
+};
